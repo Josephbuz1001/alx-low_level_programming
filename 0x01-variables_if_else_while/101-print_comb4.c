1c5d1d0788be297525a3220c1ec48c5d0d7ad7b5
Ikenna Elechi <ikennajose@gmail.com>
2022-09-09 20:52:48 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,26 +11,19 @@ int main(void)
 	int d1;
 	int d2;
 	int d3;
-	int d4;
 
 	for (d1 = 0; d1 < 8; d1++)
 	{
 		for (d2 = d1 + 1; d2 < 9; d2++)
 		{
-			for (d3 = d2 + 1; d3 < 10; d3++) 
-			{
-				for (d4 = d3 + 1; d4 < 10; d4++) 
-				{
+			for (d3 = d2 + 1; d3 < 10; d3++)
 					putchar(d1 + '0');
 					putchar(d2 + '0');
 					putchar(d3 + '0');
-					putchar(d4 + '0');
-					if (d1 == 6 && d2 == 7 && d3 == 8 && d4 == 9)
+				if (d1 == 7 && d2 == 8 && d3 == 9)
 						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+				putchar(',');
+				putchar(' ');
 		}
 	}
 	putchar('\n');
