91daa1518e9b33c76d7c077f1fb1be0e15dc52ca
Ikenna Elechi <ikennajose@gmail.com>
2022-09-09 19:53:56 -0700

    Print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,9 +6,9 @@ int main(void)
 	int d1;
 	int d2;
 
-	for (d1 = 0; d1 <= 9; d1++) 
+	for (d1 = 0; d1 <= 9; d1++)
 	{
-		for (d2 = n; d2 <= 9; d2++) 
+		for (d2 = n; d2 <= 9; d2++)
 		{
 			putchar(d1 + '0');
 			putchar(d2 + '0');
@@ -17,7 +17,7 @@ int main(void)
 				continue;
 			else if (d1 == 8 && d2 == 9)
 				continue;
-		
+
 			putchar(',');
 			putchar(' ');
 		}
