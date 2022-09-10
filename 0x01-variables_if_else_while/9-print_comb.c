547fccab04a486c38aad81f2276e0b6cea7dd01f
Ikenna Elechi <ikennajose@gmail.com>
2022-09-09 18:16:17 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main()
+{
+	int num;
+
+	for (num = 0; num <= 9; num++) {
+		putchar(num + '0');
+		if (num == 9)
+			contunue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
