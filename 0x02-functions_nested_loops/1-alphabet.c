792364b2e50e5b36664ecd38de531fc23c47f943
Ikenna Elechi <ikennajose@gmail.com>
2022-09-13 23:55:35 -0700

    Task 1'

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet -  function that prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - function that prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0
  */
+
 void print_alphabet(void)
 {
 	char b;
