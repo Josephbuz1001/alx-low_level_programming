0dcedf66d15713038232da18c148b6c89118e8ed
Ikenna Elechi <ikennajose@gmail.com>
2022-09-19 13:30:11 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - parameter and updates the value it points to 98
+ * @n: integer value
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
