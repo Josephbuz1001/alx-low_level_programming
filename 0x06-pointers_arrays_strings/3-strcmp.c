27162aaf9b087e6e3bc4bc6bc32b33f6ff04956a
Ikenna Elechi <ikennajose@gmail.com>
2022-09-22 05:27:50 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: 0 if s1 and s2 are equals
+ * another number if not
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, jk = 0;
+
+	while (jk == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + 1) == '\0'))
+			break;
+		jk = *(s1 + 1) - *(s2 + i);
+		i++;
+	}
+	return (jk);
+}
