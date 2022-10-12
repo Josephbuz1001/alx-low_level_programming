150626bc799ee21d9972602927e6ae7e26ea27a2
Ikenna Elechi <ikennajose@gmail.com>
2022-10-13 00:08:49 +0100

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,15 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name
-* @name: name to print
-* @f: function to print with
-*
-* Return: void
-*/
+ * print_name - function that prints a name
+ * @name: pointer to name
+ * @f: function pointer
+ *
+ * Reture: void
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	(*f)(name);
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
